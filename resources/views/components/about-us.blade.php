06d87b655be6ae38b2b276321825dbed5cd38533
DevKennySoriano <devkenynsoriano@gmail.com>
2026-01-30 08:21:36 +0800

    Kape't Bahala Na

--- resources/views/components/about-us.blade.php
+++ resources/views/components/about-us.blade.php
@@ -9,8 +9,10 @@
 				<div class="ms-md-2 ms-lg-5">
 					<span class="text-muted">Our Story</span>
 					<h2 class="display-5 fw-bold">About Us</h2>
-					<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
-					<p class="lead mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+					<p class="lead">Kape’t Bahala Na works as a brand name because it's deeply FIlipino,emotional and memorable. It implies that your coffee is there for people during busy days,tough decisions,late nights or moments when you just let things be. 
+
+</p>
+					<p class="lead mb-0">"kape muna bahala na yung iba".</p>
 				</div>
 			</div>
 		</div>
